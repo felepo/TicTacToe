6760f1b884444c2f7f229b5305b177f1ecc5d741
Fernando Poncio <fecrackle@gmail.com>
2017-11-15 00:04:18 -0600

    Programando comportamiento de botones.

--- app/src/main/java/com/felepo/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/felepo/tic_tac_toe/MainActivity.java
@@ -2,12 +2,84 @@ package com.felepo.tic_tac_toe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
 
+    private int jugadores;
+    private int CASILLAS[];
+    private Partida partida;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se inicia el arreglo de CASILLAS y se almacenan los id's
+        CASILLAS = new int[9];
+
+        CASILLAS[0] = R.id.a1;
+        CASILLAS[1] = R.id.a2;
+        CASILLAS[2] = R.id.a3;
+
+        CASILLAS[3] = R.id.b1;
+        CASILLAS[4] = R.id.b2;
+        CASILLAS[5] = R.id.b3;
+
+        CASILLAS[6] = R.id.c1;
+        CASILLAS[7] = R.id.c2;
+        CASILLAS[8] = R.id.c3;
+    }
+
+    public void jugar(View vista)
+    {
+        //Antes de empezar a jugar:
+
+        //PASO 1: Limpiar el tablero
+        ImageView imagen;
+
+            //Asignar la imagen de casilla a cada una de las casillas del array.
+        for( int casilla : CASILLAS )
+        {
+            imagen = (ImageView) findViewById(casilla);
+            imagen.setImageResource(R.drawable.casilla);
+        }
+
+        //PASO 2: Identificar el número de jugadores
+        jugadores = 1;
+
+            //Si el botón de dos jugadores es presionado, la variable jugadores se reescribe a 2.
+        if( vista.getId() == R.id.dosJugadores )
+        {
+            jugadores = 2;
+        }
+
+        //PASO 3: Detectar la dificultad seleccionada.
+        RadioGroup grupoDificultad = (RadioGroup) findViewById(R.id.grupoDificultad);
+        int id = grupoDificultad.getCheckedRadioButtonId();
+
+        int dificultad = 0;
+        if( id == R.id.normal )
+        {
+            dificultad = 1;
+        }
+        else
+        {
+            if (id == R.id.dificil)
+            {
+                dificultad = 2;
+            }
+        }
+
+        //PASO 4: Inhabilitar los botones mientras dure la partida
+        partida = new Partida(dificultad); //Se crea una nueva partida con dificultad
+
+        ((Button) findViewById(R.id.unJugador)).setEnabled(false);
+        ((RadioGroup) findViewById(R.id.grupoDificultad)).setAlpha(0);
+        ((Button) findViewById(R.id.dosJugadores)).setEnabled(false);
     }
 }
---
+++ app/src/main/java/com/felepo/tic_tac_toe/Partida.java
@@ -0,0 +1,15 @@
+package com.felepo.tic_tac_toe;
+
+/**
+ * Created by Fernando Poncio on 14/11/2017.
+ */
+
+public class Partida
+{
+    public final int dificultad;
+
+    public Partida(int dificultad)
+    {
+        this.dificultad = dificultad;
+    }
+}
