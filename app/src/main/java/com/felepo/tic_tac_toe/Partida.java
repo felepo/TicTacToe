426a9d902bdd8f774ee740a4617b101c8bca9be3
Fernando Poncio <fecrackle@gmail.com>
2017-11-17 00:01:23 -0600

    Estableciendo reglas de empate del juego.

--- app/src/main/java/com/felepo/tic_tac_toe/Partida.java
+++ app/src/main/java/com/felepo/tic_tac_toe/Partida.java
@@ -11,6 +11,8 @@ public class Partida
     public final int dificultad;
     public int jugador;
     private int casillasOcupadas[];
+    private final int COMBINACIONES[][] = {{0,1,2},{3,4,5},{6,7,8},
+            {0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     public Partida(int dificultad)
     {
@@ -38,14 +40,38 @@ public class Partida
         return true;
     }
 
-    public void cambiarTurno()
+    public int cambiarTurno()
     {
+        boolean empate = true;
+
+        //Verificando las combinaciones de victoria del juego
+        for( int i = 0; i < COMBINACIONES.length; i++ )
+        {
+            for( int pos : COMBINACIONES[i] )
+            {
+                //System.out.println("Valor en posición " + i + " " + casillasOcupadas[pos]);
+
+                if( casillasOcupadas[pos] == 0 )
+                {
+                    empate = false;
+                }
+            }
+        }
+
+        if( empate )
+        {
+            return 3;
+        }
+
+        //Con esto se cambia el turno
         jugador++;
 
         if( jugador > 2 )
         {
             jugador = 1;
         }
+
+        return 0;
     }
 
     public int inteligenciaArtificial()
